14b50c5c60defe55a2894a5fc69e219dce111f02
Mathi03 <mathi031@hotmail.com>
2023-01-24 12:21:35 -0500

    new Change

--- src/Setting.js
+++ src/Setting.js
@@ -1,7 +1,3 @@
-let url = "../catalogo_api_blank/catalogo_api_blank.php";
-var URLActual = window.location.hostname;
-if (URLActual === "localhost" || URLActual === "127.0.0.1") {
-  url = "http://localhost:3000/api.php";
-}
+const url = "../catalogo_api_blank/catalogo_api_blank.php";
 
 export default url;
