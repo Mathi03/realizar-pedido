7456a3f7ebe7c6c8938ca13b444c807964173632
Mathi03 <mathi031@hotmail.com>
2023-01-24 12:17:35 -0500

    new Change

--- src/Setting.js
+++ src/Setting.js
@@ -1,15 +1,7 @@
-let url = "";
+let url = "../catalogo_api_blank/catalogo_api_blank.php";
 var URLActual = window.location.hostname;
 if (URLActual === "localhost" || URLActual === "127.0.0.1") {
   url = "http://localhost:3000/api.php";
-}
-if (
-  URLActual === "catalogo.footloose.pe" ||
-  URLActual === "192.168.3.70" ||
-  URLActual === "sip.footloose.pe" ||
-  URLActual === "scriptcase.footloose.pe"
-) {
-  url = "../catalogo_api_blank/catalogo_api_blank.php";
 }
 
 export default url;
